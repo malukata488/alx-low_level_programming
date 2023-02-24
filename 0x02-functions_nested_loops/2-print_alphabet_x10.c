d5dbce2045dd2f4400e1bdc5bb1d18d08bd21c49
malukata488 <malukatagallet@gamil.com>
2023-02-24 07:32:32 -0800

    2-largest_number.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,12 +12,12 @@ void print_alphabet_x10(void)
 	        int i = 0;
 
 		while (i <= 10)
-		{
+	       	{
 		for (c = 'a'; c <= 'z'; c++)
 		{
 		_putchar(c);
 		}
-		_putchar('\n');
+		_putchar('\n')
 		i++;
 		}
-}
+}		

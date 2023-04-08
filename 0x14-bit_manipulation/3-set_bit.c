45baac2e6d307ff8024b319a48d2450dc9b2901b
malukata488 <malukatagallet@gmail.com>
2023-04-08 07:29:59 -0700

    set bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,7 +10,7 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= (sifeof(unsigned long int) * 8))
+	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
 
 	*n ^= (1 << index);
